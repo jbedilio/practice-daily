53d7d86c318c55ca106d460d3250745301f8eeee
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-09-22 00:03:43 -0400

    exercises

---
+++ paramete.js
@@ -0,0 +1,20 @@
+console.log(process.argv[2]);
+console.log(process.argv[3]);
+
+if (process.argv[2] == process.argv[3]) {
+
+console.log('they are equal');
+
+} else {
+    console.log('they are not equal')
+};
+
+
+console.log(process.argv[4]);
+console.log(process.argv[5]);
+
+if (process.argv[4] < process.argv[5]) {
+    console.log("less than")
+} else {
+    console.log("equal or greater baby")
+};
---
+++ sriracha.js
@@ -0,0 +1,25 @@
+
+
+
+console.log("Sriracha goes great on . . . nothing!");
+
+const name = "josh";
+
+console.log(name);
+
+console.log(Math.pow(2, 12));
+
+console.log('fucking cool');
+
+setTimeout (() => {
+
+    console.log('This was 3 seconds late');
+
+},3 * 1000);
+
+console.log(process.argv.length);
+
+var myName = "Josh";
+
+console.log(myName);
+
