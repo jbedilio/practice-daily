086b7a9d6a812488d1a6f660f94849b926f7030b
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-09-22 22:48:52 -0400

    node fs.write function & calculator

---
+++ args.js
@@ -0,0 +1,33 @@
+
+
+/*console.log(process.argv);
+if (process.argv[3] === process.argv[2] ) {
+  console.log(true)
+}  else {
+  console.log(false)
+}*/
+
+/*console.log(process.argv);
+
+if (process.argv[3] == process.argv[2]) {
+    console.log('True');
+} else {
+    console.log('False');
+}*/
+
+/*console.log(process.argv);
+
+if (process.argv[2] == process.argv[3]) {
+    console.log("that is fo riz");
+} else {
+    console.log("that is some shiz");
+}*/
+
+console.log(process.argv);
+
+if (process.argv[2] !==! process.argv[3]) {
+    console.log("what's going to happen?");
+} else {
+    console.log("we'll see, I guess");
+}
+
---
+++ write.js
@@ -0,0 +1,43 @@
+var fs = require('fs');
+
+fs.writeFile('movies.txt', 'Inception, Die Hard', error => {
+
+
+    if(error){
+
+        return console.log(error);
+        
+    }
+
+    console.log('movies.txt was updated');
+
+});
+
+
+var fs = require('fs');
+
+fs.writeFile('movies2.txt', 'Dodgeball, Blow, The Doors', error => {
+
+    if (error){
+
+        console.log(shiz);
+
+    }
+
+    console.log('Updated the movies2.txt file cash money pimp maching');
+});
+
+
+
+var fs = require('fs');
+
+fs.writeFile('movies3.txt', 'It, The Interview, Four Rooms', error => {
+
+    if(error){
+
+        console.log(wowza);
+
+    }
+    console.log('movies3.txt update cash money pimp machine');
+
+});
