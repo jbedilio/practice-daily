6930954df7ee5f8a12ead82694ace513b6331a75
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-10-17 21:57:07 -0400

    mongojs

--- mongojsServer.js
+++ mongojsServer.js
@@ -49,3 +49,95 @@ app.get('/all/weight', (req, res) => {
         }
     });
 });
+
+var databaseUrl = 'zoodb';
+var collections = ['animals'];
+
+var db = mongojs(databaseUrl, collections);
+
+db.on('error', (error) => {
+    console.log('db error', error);
+});
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+app.get('/all', (req, res) => {
+    db.animals.find({}, (error, data) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            res.json(data);
+        }
+    });
+});
+
+app.get('/all/name', (req, res) => {
+    db.animals.find().sort({name:1}, (error, data) => {
+        if (error) {
+            throw error;
+        }
+        else {
+            res.json(data);
+        }
+    });
+});
+
+app.get('/all/weight', (req, res) => {
+    db.animals.find().sort({weight:-1}, (error, data) => {
+        if (error) {
+            throw error;
+        }
+        else {
+            res.json(data);
+        }
+    });
+});
+
+var databaseUrl = 'zoodb';
+var collections = ['animals'];
+
+var db = mongojs(databaseUrl, collections);
+
+db.on('error', (error) => {
+    console.log(error);
+});
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+app.get('/all', (req, res) => {
+    db.animals.find({}, (error, data) => {
+        if (error) {
+            throw error;
+        }
+        else {
+            res.json(data);
+        }
+    });
+});
+
+app.get('/all/name', (req, res) => {
+    db.animals.find().sort({name:1}, (error, data) => {
+        if (error) {
+            throw error;
+        }
+        else {
+            res.json(data);
+        }
+    });
+});
+
+app.get('/all/weight', (req, res) => {
+    db.animals.find().sort({weight:-1}, (error, data) => {
+        if (error) {
+            throw error;
+        }
+        else {
+            res.json(data);
+        }
+    });
+});
